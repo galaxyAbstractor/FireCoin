e4e8408e52d9a19ac6b871ae553f19031ab19735
galaxyAbstractor <vigge.swe@gmail.com>
2011-05-13 21:36:55 +0200

    metatag existance check

--- chrome/content/firecoin/firecoinOverlay.js
+++ chrome/content/firecoin/firecoinOverlay.js
@@ -1,39 +1,57 @@
-// @TOREAD: http://forums.mozillazine.org/viewtopic.php?f=19&t=952905
+// @TOREMEMBER extensions.yourthing.so.it.is.correctly.namespaced.tabs = []; ....tabs.push(tab); if (...tabs.indexOf(tab) > -1) { /* in there */ }
 
 var FireCoin = {
 	onclick: function() {
 	},
 	
-	contentListener: function(evt) {
-            alert(evt);
-	},
-	
+	/**
+	 * Get the content of a metatag
+	 */
 	getMetaContents:function(mn){
-		var m = document.getElementsByTagName('meta');
+		var m = content.document.getElementsByTagName('meta');
 		
 		for(var i in m){
-			if(m[i][i].name == mn){
+			if(m[i].name == mn){
 				return m[i].content;
 			}
 		}
+		// meta tag not found
 		return "nope.avi";
 	}
 }
 
 document.addEventListener("DOMContentLoaded", function(e) { 
-	var document = e.target;
+	// The loaded document
+	var doc = e.target;
 	
-	if (!document.location)
+	// if it hasn't a location, stop
+	if (!doc.location)
         return;
  
-    var href = document.location.href;
+    // get the URL
+    var href = doc.location.href;
  
-    if (!/(http)/i.test(href))
+    // if it doesn't start with http:// we'll skip it
+    if (doc.location.protocol != "http:")
         return;
-        
+    
+    // Prevents running the event multiple times per load
+    // only run the update if the loaded URL is the same as the current selected tab UTL    
     if(href != content.document.location.href)
     	return;
     	
-    FireCoin.contentListener(content.document.location.href);
+    // Get the button from the XUL
+    var button = document.getElementById("firecoin-button");
+    
+    // If the meta is set
+    if(FireCoin.getMetaContents("bitcoin") != "nope.avi"){
+    	// Set the active class
+    	button.setAttribute("class", "toolbarbutton-1 firecoin-button");
+    	alert("found");
+    } else {
+    	// else set the inactive class
+    	button.setAttribute("class", "toolbarbutton-1 firecoin-button-inactive");
+    	alert("not found");
+    }
             
  }, false);
--- readme.rst
+++ readme.rst
@@ -2,4 +2,5 @@ Makes it easier for you to donate Bitcoins to siteowners with firefox.
 
 Changelog
 =======
-12 May, 2011: Created structure and sample code to work from
+* 13 May, 2011: Detects the meta bitcoin now and sets the button to active if it exists. Does not update the button on tab change yet.
+* 12 May, 2011: Created structure and sample code to work from
